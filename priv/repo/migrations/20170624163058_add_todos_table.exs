fc5fd0507b580b855b9078ef468b12bca3d89b07
Sasank Yadavalli <yadavallisasank@gmail.com>
2017-06-24 22:30:08 +0530

    Add todo migration to PhoenixTodo.Repo.Migrations.AddTodosTable

---
+++ priv/repo/migrations/20170624163058_add_todos_table.exs
@@ -0,0 +1,18 @@
+defmodule PhoenixTodo.Repo.Migrations.AddTodosTable do
+  use Ecto.Migration
+
+  def up do
+    create table(:todos, primary_key: false) do
+      add :id, :binary_id, primary_key: false
+      add :todo, :string
+      add :description, :string
+      add :completed, :boolean
+
+      timestamps()
+    end
+  end
+
+  def down do
+    drop table(:todos)
+  end
+end
